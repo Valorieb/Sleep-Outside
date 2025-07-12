d07dc10583758e69f4517341f2084b196bdc3a6a
benwasden <was24001@byui.edu>
2025-07-12 07:18:44 -0700

    Ben Wasden week 2 individual finished

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+const dataSource = new ProductData("tents");
+
+const element = document.querySelector(".product-list");
+
+const tentList = new ProductList("tents", dataSource, element);
+tentList.init();
