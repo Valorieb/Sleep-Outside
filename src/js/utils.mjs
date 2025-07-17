eb26ee16619dd46e26146db51d758ef91a07af09
benwasden <was24001@byui.edu>
2025-07-17 15:18:55 -0700

    Week 3 team basic

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -52,3 +52,27 @@ export function calcDiscount(finalPrice, suggestedPrice) {
   const discount = ((suggestedPrice - finalPrice) / suggestedPrice) * 100;
   return discount.toFixed(0);
 }
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if (callback) {
+    callback(data);
+  }
+}
+
+export async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const headerElement = document.querySelector("#main-header");
+
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+  const footerElement = document.querySelector("#main-footer");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
+}
