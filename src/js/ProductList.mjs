4212e591978c9d560ba94afb1a777ee928193db1
benwasden <was24001@byui.edu>
2025-07-19 11:07:56 -0700

    Week 3 individual

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -7,8 +7,10 @@ export default class ProductList {
     this.listElement = listElement;
   }
   async init() {
-    const list = await this.dataSource.getData();
+    const list = await this.dataSource.getData(this.productCategory);
     this.renderList(list);
+    const title = this.productCategory.charAt(0).toUpperCase() + this.productCategory.slice(1);
+    document.querySelector(".title").textContent = title.replace("-", " ");
   }
   renderList(productList) {
     renderListWithTemplate(productCardTemplate, this.listElement, productList);
@@ -22,7 +24,7 @@ function productCardTemplate(product) {
   );
   return `<li class="product-card">
     <a href="product_pages/?product=${product.Id}">
-      <img src="${product.Image}" alt="Image of ${product.Brand.Name}">
+      <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Brand.Name}">
       <h2 class="card__brand">${product.Brand.Name}</h2>
       <h3 class="card__name">${product.Name}</h3>
       <p class="product-card__price">$${product.FinalPrice}</p>
